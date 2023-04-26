7cf6e93dbb4dc761cac5c21d345e8c105b44cb94
MoulineRo <simonovandsim@gmail.com>
2023-04-26 20:08:57 +0300

    2

--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from faker import Faker
 
-
-
 fake = Faker()
 fname = fake.name()
 fage = fake.random_int(min=30, max=50)
@@ -12,18 +10,16 @@ class Student(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField(default=0)
 
-bar = Student.objects.create(name=fname,age=fage)
 
+bar = Student.objects.create(name=fname, age=fage)
 
 
 class Students(models.Model):
-
     name = models.CharField(max_length=50)
     age = models.IntegerField(default=0)
+
 
 # users = [fake.name() for i in range(10)]
 # fage = [fake.random_int(min=30, max=50) for y in range(10)]
 for _ in range(10):
-    s=Students.objects.create(name=fake.name(),age=fake.random_int(min=30, max=50))
-
-
+    s = Students.objects.create(name=fake.name(), age=fake.random_int(min=30, max=50))
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
-from faker import Faker
-from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Student,Students
+from django.shortcuts import render
+from faker import Faker
 
+from .models import Student, Students
 
 fake = Faker()
 fname = fake.name()
@@ -12,17 +12,18 @@ fage = fake.random_int(min=30, max=50)
 def student(request):
     latest1 = Student.objects.latest('id').name
     latest2 = Student.objects.latest('id').age
-    res=latest1+' '+str(latest2)
+    res = latest1 + ' ' + str(latest2)
     return HttpResponse(res)
+
+
 def index(request):
     return render(request, "index.html")
 
 
-
 def postuser(request):
     age = int(request.POST.get("age", 1))
-    res=[]
-    if age>0 and age <=100:
+    res = []
+    if age > 0 and age <= 100:
         for _ in range(age):
             s = Students.objects.create(name=fake.name(), age=fake.random_int(min=30, max=50))
             res.append(s.name + ' ')
